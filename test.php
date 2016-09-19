eb99b689a1e08ed3b65520848a5879e6e52eded7
surojdesignoffshore <suroj.designoffshore@gmail.com>
2016-09-19 11:15:01 +0545

    expression matching using preg_match() function to see if there is exact same pattern as we search

--- test.php
+++ test.php
@@ -1,6 +1,34 @@
 <?php
-	include 'header_inc.php';
-	include_once 'header_inc.php';
-	
-	echo $var1;
+	$string = "Thisisastring";
+	if (preg_match('/a/', $string))
+	{
+		echo "Match Found";
+	}
+	else
+	{
+		echo "No Match Found";
+	}
+	echo "<br />";
+	echo "The following is result of function<br />";
+
+	function has_space($string)
+	{
+		if (preg_match('/ /', $string))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	if (has_space($string))
+	{
+		echo "Has at least one space";
+	}
+	else
+	{
+		echo "Has no spaces";
+	}
 ?>
